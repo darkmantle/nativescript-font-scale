bafd0c3c76d8f91fe612c7d058a59dd1a6c62e33
David Hopson <mantle@hotmail.co.uk>
2019-08-07 20:36:20 +0100

    Fixing lint issues & README

--- src/font-scale.ios.ts
+++ src/font-scale.ios.ts
@@ -7,12 +7,12 @@ export class FontScale extends Common {
 
         this.on("loaded", () => {
             const ios: UILabel = this.ios;
-        
+
             const newFont = new Font(this.style.fontFamily, this.style.fontSize, null, this.style.fontWeight);
             ios.font = UIFontMetrics.defaultMetrics.scaledFontForFont(newFont.getUIFont(null));
-            
+
             ios.adjustsFontForContentSizeCategory = true;
 
-        })
+        });
     }
 }
