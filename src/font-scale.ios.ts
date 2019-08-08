2166e81bba14509c4393e3ca06976a8ba5c273b9
David Hopson <mantle@hotmail.co.uk>
2019-08-08 12:28:27 +0100

    Fixing TabStripItem

--- src/font-scale.ios.ts
+++ src/font-scale.ios.ts
@@ -10,6 +10,10 @@ export class Label extends LabelCommon {
         this.on("loaded", () => {
             const ios: UILabel = this.ios;
 
+            if (!this.style.fontFamily || this.style.fontSize || this.style.fontWeight) {
+                return;
+            }
+
             const newFont = new Font(this.style.fontFamily, this.style.fontSize, null, this.style.fontWeight);
             ios.font = UIFontMetrics.defaultMetrics.scaledFontForFont(newFont.getUIFont(null));
 
@@ -25,13 +29,17 @@ export class Button extends ButtonCommon {
 
         this.on("loaded", () => {
             const btn: UIButton = this.ios;
-            const ios: UILabel = btn.titleLabel
+            const ios: UILabel = btn.titleLabel;
+
+            if (!this.style.fontFamily || this.style.fontSize || this.style.fontWeight) {
+                return;
+            }
 
             const newFont = new Font(this.style.fontFamily, this.style.fontSize, null, this.style.fontWeight);
             ios.font = UIFontMetrics.defaultMetrics.scaledFontForFont(newFont.getUIFont(null));
 
             ios.adjustsFontForContentSizeCategory = true;
-        })
+        });
     }
 
 }
@@ -43,10 +51,14 @@ export class TabStripItem extends TabStripItemCommon {
         this.on("loaded", () => {
             const item: UITabBarItem = this.nativeView;
 
-            let attrs: NSDictionary<any, any> = NSMutableDictionary.new();
-            const newFont = new Font("SF UI Text Medium", 17, null, FontWeight.NORMAL);
-            attrs.setValueForKey(newFont.getUIFont(null), NSFontAttributeName);
+            const test: NSDictionary<String, any> = item.titleTextAttributesForState(UIControlState.Normal);
+            const font: UIFont = test.valueForKey("NSFont");
 
+            const attrs: NSDictionary<any, any> = NSMutableDictionary.new();
+            const newFont = new Font(font.familyName, font.pointSize, null, FontWeight.NORMAL);
+
+            const scaledFont = UIFontMetrics.defaultMetrics.scaledFontForFont(newFont.getUIFont(null));
+            attrs.setValueForKey(scaledFont, NSFontAttributeName);
             item.setTitleTextAttributesForState(attrs, UIControlState.Normal);
         });
     }
