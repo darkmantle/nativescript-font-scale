a295801ea545f5e22cd27cb48e000a5ebc119e82
David Hopson <mantle@hotmail.co.uk>
2019-08-08 13:55:08 +0100

    Added property to disable scale

--- src/font-scale.common.ts
+++ src/font-scale.common.ts
@@ -1,19 +1,47 @@
 import { Label } from "tns-core-modules/ui/label";
 import { Button } from "tns-core-modules/ui/button";
 import { TabStripItem } from "tns-core-modules/ui/tab-navigation-base/tab-strip-item";
+import { Property, booleanConverter } from "tns-core-modules/ui/core/view";
 
 export class LabelCommon extends Label {
+    public scale: boolean;
     constructor() {
         super();
     }
 }
 
+export const buttonScaleProperty = new Property<ButtonCommon, boolean>({
+    name: "scale",
+    defaultValue: true,
+    valueConverter: booleanConverter,
+    affectsLayout: true
+});
+
+export const labelScaleProperty = new Property<LabelCommon, boolean>({
+    name: "scale",
+    defaultValue: true,
+    valueConverter: booleanConverter,
+    affectsLayout: true
+});
+
+export const tabStripScaleProperty = new Property<TabStripItemCommon, boolean>({
+    name: "scale",
+    defaultValue: true,
+    valueConverter: booleanConverter,
+    affectsLayout: true
+});
+
+
 export class ButtonCommon extends Button {
+    public scale: boolean;
     constructor() {
         super();
     }
 }
 
 export class TabStripItemCommon extends TabStripItem {
-
+    public scale: boolean;
+    constructor() {
+        super();
+    }
 }
--- src/font-scale.ios.ts
+++ src/font-scale.ios.ts
@@ -1,7 +1,10 @@
 import { Font, FontWeight } from "tns-core-modules/ui/styling/font";
 
-import { ButtonCommon, LabelCommon, TabStripItemCommon } from "./font-scale.common";
-import { fontFamilyProperty } from "tns-core-modules/ui/page/page";
+import { ButtonCommon, LabelCommon, TabStripItemCommon, labelScaleProperty, buttonScaleProperty, tabStripScaleProperty } from "./font-scale.common";
+
+labelScaleProperty.register(LabelCommon);
+buttonScaleProperty.register(ButtonCommon);
+tabStripScaleProperty.register(TabStripItemCommon);
 
 export class Label extends LabelCommon {
     constructor() {
@@ -9,17 +12,18 @@ export class Label extends LabelCommon {
 
         this.on("loaded", () => {
             const ios: UILabel = this.ios;
-
-            if (!this.style.fontFamily || this.style.fontSize || this.style.fontWeight) {
-                return;
-            }
+            
+            if (!this.scale) return;
 
             const newFont = new Font(this.style.fontFamily, this.style.fontSize, null, this.style.fontWeight);
             ios.font = UIFontMetrics.defaultMetrics.scaledFontForFont(newFont.getUIFont(null));
 
             ios.adjustsFontForContentSizeCategory = true;
-
         });
+    }
+
+    [labelScaleProperty.getDefault]() {
+        return true;
     }
 }
 
@@ -31,15 +35,17 @@ export class Button extends ButtonCommon {
             const btn: UIButton = this.ios;
             const ios: UILabel = btn.titleLabel;
 
-            if (!this.style.fontFamily || this.style.fontSize || this.style.fontWeight) {
-                return;
-            }
+            if (!this.scale) return;
 
             const newFont = new Font(this.style.fontFamily, this.style.fontSize, null, this.style.fontWeight);
             ios.font = UIFontMetrics.defaultMetrics.scaledFontForFont(newFont.getUIFont(null));
 
             ios.adjustsFontForContentSizeCategory = true;
         });
+    }
+
+    [buttonScaleProperty.getDefault]() {
+        return true;
     }
 
 }
@@ -50,6 +56,8 @@ export class TabStripItem extends TabStripItemCommon {
 
         this.on("loaded", () => {
             const item: UITabBarItem = this.nativeView;
+            
+            if (!this.scale) return;
 
             const test: NSDictionary<String, any> = item.titleTextAttributesForState(UIControlState.Normal);
             const font: UIFont = test.valueForKey("NSFont");
@@ -59,7 +67,12 @@ export class TabStripItem extends TabStripItemCommon {
 
             const scaledFont = UIFontMetrics.defaultMetrics.scaledFontForFont(newFont.getUIFont(null));
             attrs.setValueForKey(scaledFont, NSFontAttributeName);
+            
             item.setTitleTextAttributesForState(attrs, UIControlState.Normal);
         });
+    }
+
+    [tabStripScaleProperty.getDefault]() {
+        return true;
     }
 }
